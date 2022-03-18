7037734e2525c4711e353a74b2960f7285e266e4
FilipS90 <filip.stojiljkovic90@yahoo.com>
2022-03-18 21:17:11 +0100

    Update test

--- common programming concepts/src/main.rs
+++ common programming concepts/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let x = (1,5,"Hello", 'a');
+    println!("{}", x.3);
+
+    let q = [3; 5];
+    println!("{}", q[2]);
+
+    let number : u32 = 30;
+
+    if number > 40 {
+        println!("condition was true");
+    } else if number < 100 {
+        println!("blah");
+    } else {
+        println!("condition was false");
+    }
+
+    println!("The result of someMethod is: {}", some_method());
 }
